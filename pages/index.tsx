4c3bb85e6881eb841f2df5bf479d190bbe3cb4c9
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-04-10 18:18:33 +0900

    Add Google Analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import type { NextPage } from 'next'
 import {isMobile} from 'react-device-detect';
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as gtag from '../lib/gtag';
+
 import PcPage from './pc'
 import MobilePage from './mobile'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     isMobile ? <MobilePage /> : <PcPage />
   )
